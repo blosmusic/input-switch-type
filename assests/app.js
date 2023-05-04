969bc17615f577f94b1551b52b533da0583e4052
BLOS <blosguitarist@gmail.com>
2023-05-04 22:30:43 +0100

    switch statement added

--- assests/app.js
+++ assests/app.js
@@ -1,6 +1,20 @@
 const selectedOptions = document.getElementById("audio-source");
 
 selectedOptions.addEventListener("change", (event) => {
-const selectedOptionValue = event.target.value;
-console.log("Value Selected:", selectedOptionValue);
+  const selectedOptionValue = event.target.value;
+
+  switch (selectedOptionValue) {
+    case "mono":
+      console.log("Mono");
+      break;
+    case "stereo":
+      console.log("Stereo");
+      break;
+    case "midi":
+      console.log("Midi");
+      break;
+    default:
+      console.log("No value selected");
+      break;
+  }
 });
