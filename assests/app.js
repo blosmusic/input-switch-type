aa8b3edcd11d16f5bb9bf83664417d8982fc5df5
BLOS <blosguitarist@gmail.com>
2023-05-07 02:05:09 +0100

    Revert "audio switching"
    This reverts commit 5a9fd97ba17747d1b7378a96ea282b29690fa28c.

--- assests/app.js
+++ assests/app.js
@@ -1,54 +1,36 @@
 const select = document.getElementById("audioDevices");
 const selectedOptions = document.getElementById("audio-source");
 
-let midiInput;
-const synth = new Tone.PolySynth().toDestination();
-const now = Tone.now();
-
-////////////////////////////
-
 // Create a new AudioContext
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let mediaStream;
-let mediaStreamSource;
-let audioDestination;
+let sourceNode;
+
+let midiInput;
+const synth = new Tone.PolySynth().toDestination();
+const now = Tone.now();
 
 // Handle device selection change
 select.addEventListener("change", async () => {
   const selectedDeviceId = select.value;
 
+  // Check if there is an active MediaStream and disconnect it
+  if (mediaStream && sourceNode) {
+    sourceNode.disconnect();
+    mediaStream.getTracks().forEach((track) => track.stop());
+  }
+
   try {
-    // Check if there is an active MediaStream and stop it
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => track.stop());
-    }
-
-    // Check if there is an active MediaStreamSource and disconnect it
-    if (mediaStreamSource) {
-      mediaStreamSource.disconnect(audioDestination);
-      mediaStreamSource = null;
-    }
-
     // Create a MediaStream using the selected audio device
     mediaStream = await navigator.mediaDevices.getUserMedia({
       audio: { deviceId: selectedDeviceId },
     });
 
     // Create a MediaStreamAudioSourceNode
-    mediaStreamSource = audioContext.createMediaStreamSource(mediaStream);
+    sourceNode = audioContext.createMediaStreamSource(mediaStream);
 
-    // Check if there is an active AudioDestinationNode
-    if (!audioDestination) {
-      audioDestination = audioContext.destination;
-    }
-
-    // Connect the source node to the destination node
-    mediaStreamSource.connect(audioDestination);
-
-    // Resume the audio context (required on some browsers)
-    if (audioContext.state === "suspended") {
-      await audioContext.resume();
-    }
+    // Connect the source node to the audio context destination
+    sourceNode.connect(audioContext.destination);
   } catch (error) {
     console.error("Error accessing audio device:", error);
   }
@@ -82,8 +64,6 @@ navigator.mediaDevices
   .catch((error) => {
     console.error("Error accessing audio device:", error);
   });
-
-////////////////////////////
 
 document.body.addEventListener("click", async () => {
   await Tone.start();
